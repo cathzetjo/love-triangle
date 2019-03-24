d27ed5baa9b2ff226f21f515acc59d590e4cec4b
Katerina Zhadko <katerinkaj@icloud.com>
2019-03-24 15:34:46 +0300

    feat:add variables and cycle to count number of triangles

--- src/index.js
+++ src/index.js
@@ -3,5 +3,19 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+
+    let count = 0;
+    for (let i = 0; i < preferences.length; i++) {
+        let firstSpichonee = i + 1;
+        let firstSpichoneeLoves = preferences[i];
+        let nextSpichoneeLoves = preferences[firstSpichoneeLoves - 1];
+        let lastSpichoneeLoves = preferences[nextSpichoneeLoves - 1];
+        if (lastSpichoneeLoves == firstSpichonee) {
+            count++;
+        }
+    }
+    return parseInt(count / 3);
 };
+
+
+
